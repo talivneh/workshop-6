fb906eafe2ce997bd3da4952422f44c8c82a3071
Tal Livneh <talivneh8@gmail.com>
2022-05-28 10:19:17 +0300

    feat: first part

--- weather.js
+++ weather.js
@@ -9,27 +9,47 @@
 // 8. BONUS - Implement the second command.
 
 // Commander usage example for your reference:
-import chalk from "chalk";
 import { Command } from "commander";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const program = new Command();
+const apiKey = process.env.WEATHER_API_KEY;
+
+async function getTemp(city, type = "c") {
+  switch (type) {
+    case "c":
+      "metric";
+      break;
+    case "f":
+      "imperial";
+      break;
+  }
+
+  const apiRes = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${type}`
+  );
+  const currentTemp = await apiRes.json();
+
+  return currentTemp.main.temp;
+}
 
 program
-  .name("cli-calc")
-  .description("The best CLI calculator")
+  .name("cli-weather")
+  .description("get current weathet in your CLI")
   .version("1.0.0");
 
 program
-  .command("add")
-  .description("Add two numbers")
-  .argument("<number>", "first operand")
-  .argument("<number>", "second operand")
-  .option("-c, --color <string>", "Result color", "white")
-  .action((firstNumber, secondNumber, options) => {
-    console.log(
-      chalk[options.color](
-        `Result: ${Number(firstNumber) + Number(secondNumber)}`
-      )
-    );
+  .command("get-temp")
+  .description("Fetch and display the temperature of a certain city")
+  .argument("<city name>", "city name")
+  // .option("-t, --type <string>", "Type of temp", "celsious")
+  .action((cityName) => {
+    getTemp(cityName).then((resault) => {
+      console.log(`Result: The current temp in ${cityName} is ${resault}`);
+    });
   });
 
 program.parse();
